4b1a74ffba5f9560e19a4b05a2d87dc78ff0dbf5
jinge <jinge1399@163.com>
2016-07-17 09:46:13 +0800

    change html

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -30,32 +30,32 @@ myAppModule.controller('layoutCtrl', function ($scope,$location,$route,$routePar
     $scope.$route = $route;
     $scope.$location = $location;
     $scope.$routeParams = $routeParams;
-    
+
     $scope.click = function () {
         $.post("about", function (data) {
         })
     }
 
     $scope.mainMenu = [
-        {page:'home',name:"首页",index:0},
-        {page:'blog',name:"博文",index:1},
-        {page:'game',name:"游戏",index:2},
-        {page:'other',name:"其他",index:3}];
+        {page:'home',name:"首页"},
+        {page:'blog',name:"博文"},
+        {page:'game',name:"游戏"},
+        {page:'other',name:"其他"}];
     
     $scope.menuClick = function (data) {
         // console.info("path : " + $location.path());
         // console.info("templateUrl : " + $route.current.templateUrl);
         // console.info("activetab : " + $route.current.activetab);
-        var mainMenu = $scope.mainMenu;
-        for(var i = 0;i < mainMenu.length;i++){
-            if(mainMenu[i].index == data){
-                if(!mainMenu[i].isSelect){
-                    mainMenu[i].isSelect = true;
-                }
-            }
-            else {
-                mainMenu[i].isSelect = false;
-            }
-        }
+        // var mainMenu = $scope.mainMenu;
+        // for(var i = 0;i < mainMenu.length;i++){
+        //     if(mainMenu[i].index == data){
+        //         if(!mainMenu[i].isSelect){
+        //             mainMenu[i].isSelect = true;
+        //         }
+        //     }
+        //     else {
+        //         mainMenu[i].isSelect = false;
+        //     }
+        // }
     }
 });
